32f8b75315a020bbd9a62af7ebb9b5ae9d8129cd
NotRyken <notryken@gmail.com>
2024-07-01 10:27:45 +0800

    Flag neo entrypoint as client-only

--- neoforge/src/main/java/dev/terminalmc/effecttimerplus/EffectTimerPlusNeoForge.java
+++ neoforge/src/main/java/dev/terminalmc/effecttimerplus/EffectTimerPlusNeoForge.java
@@ -1,11 +1,12 @@
 package dev.terminalmc.effecttimerplus;
 
 import dev.terminalmc.effecttimerplus.gui.screen.OptionsScreen;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(EffectTimerPlus.MOD_ID)
+@Mod(value = EffectTimerPlus.MOD_ID, dist = Dist.CLIENT)
 public class EffectTimerPlusNeoForge {
     public EffectTimerPlusNeoForge() {
         ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class,
